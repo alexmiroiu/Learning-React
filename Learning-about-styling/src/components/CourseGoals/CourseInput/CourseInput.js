9cebc5e3055c876d78e28fe1d0c741352fed8d03
alexmru <alexandrumiroiu@gmail.com>
2021-09-28 09:46:36 +0300

    am invatat despre css modules si styled components

--- Learning-about-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Learning-about-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,9 +24,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: isValid ? 'black' : 'red'}}>Course Goal</label>
-        <input style={{borderColor: isValid ? '#ccc' : 'red', backgroundColor: isValid ? 'transparent' : 'salmon'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
