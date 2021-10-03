607dadafb80d84be8d6851091dd372d9cfbc8d3a
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-10-03 14:09:37 +0300

    am terminat aplicatia prezentata in curs , in care s-a utilizat tot ce s-a predat pana la momentul asta

--- Food-order-app-academind/src/components/Layout/HeaderCartButton.js
+++ Food-order-app-academind/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
+    const [btnHighlighted, setBtnHighlighted] = useState(false);
+
     const cartContext = useContext(CartContext);
 
     const numberOfCardItems = cartContext.items.reduce((curNumber, item) => {
         return curNumber + item.amount
     }, 0)
 
-    return <button className={classes.button} onClick={props.btnClick}>
+    const {items} = cartContext;
+
+    const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setBtnHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnHighlighted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer)
+        }
+
+    }, [items])
+
+    return <button className={btnClasses} onClick={props.btnClick}>
         <span className={classes.icon}>
             <CartIcon></CartIcon>
         </span>
