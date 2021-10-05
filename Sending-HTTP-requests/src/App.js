15dbdf706bb9403a98d5d64f4e7abc3d04e5e8e8
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-10-05 09:35:58 +0300

    lectii despre http requests

---
+++ Sending-HTTP-requests/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import MoviesList from './components/MoviesList';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies to show</p>}
+        {isLoading && <p>LOADING PLEASE WAIT....</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
