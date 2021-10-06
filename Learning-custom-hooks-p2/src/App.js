28d0a76575f9ff9f89532bf654ac6b8b2f55aafc
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-10-06 09:36:11 +0300

    lectia custom hooks , partea 1

---
+++ Learning-custom-hooks-p2/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
