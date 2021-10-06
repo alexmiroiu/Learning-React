b570c06ea6993d4ef557f88aa5ba446d152a77ab
alexmru <alexandrumiroiu@gmail.com>
2021-10-06 18:01:47 +0300

    lectia custom hooks

--- Learning-custom-hooks-p2/src/App.js
+++ Learning-custom-hooks-p2/src/App.js
@@ -2,10 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './components/hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text})
+    }
+
+    setTasks(loadedTasks);
+  }
+
+  const httpData = useHttp({url: 'https://react-http-e2bde-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'}, transformTasks);
+  const {isLoading, error, sendRequest: fetchTasks} = httpData;
 
   useEffect(() => {
     fetchTasks();
