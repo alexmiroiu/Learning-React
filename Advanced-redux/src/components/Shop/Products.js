2da1a522789c0a462ca54a9be78689ea4c58297c
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-11-12 21:00:17 +0200

    advanced redux partea a doua

--- Advanced-redux/src/components/Shop/Products.js
+++ Advanced-redux/src/components/Shop/Products.js
@@ -1,16 +1,18 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const FAKE_PRODUCTS = [
+  {id: 'p1', price: 6, title: 'Some book', description: 'A random book'},
+  {id: 'p1', price: 9, title: 'dune', description: 'Second chapter of dune'}
+
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {FAKE_PRODUCTS.map(product => <ProductItem key={product.id} title={product.title} price={product.price} description={product.description} />)}
       </ul>
     </section>
   );
