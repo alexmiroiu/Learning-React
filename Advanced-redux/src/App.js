d8e99eb9c6c15fdea6909dfbac1a5bedce05df90
alexmru <alexandrumiroiu@gmail.com>
2021-11-17 08:56:05 +0200

    advanced redux ultima parte - thunks

--- Advanced-redux/src/App.js
+++ Advanced-redux/src/App.js
@@ -3,7 +3,7 @@ import {useSelector, useDispatch} from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 import Notification from './components/UI/Notification';
 
 let initialRender = true;
@@ -14,40 +14,20 @@ function App() {
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
 
-  useEffect( () => {
-    const sendCartData = async () => {
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [dispatch])
 
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }))
-
-      const response = await fetch('https://react-http-e2bde-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {method: 'PUT', body: JSON.stringify(cart)}, [cart]);
-
-      if(!response.ok) {
-        throw new Error('Sending cart data failed!');
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }))
-    }
-
+  useEffect(() => {
     if(initialRender) {
       initialRender = false;
       return;
     }
+    if(cart.changed) {
+      dispatch(sendCartData(cart))
+    }
 
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }))
-    })
+
   }, [cart, dispatch])
 
   return (
--- Advanced-redux/src/store/cart-slice.js
+++ Advanced-redux/src/store/cart-slice.js
@@ -4,13 +4,19 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
-        totalQuantity: 0
+        totalQuantity: 0,
+        changed: false
     },
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+         },
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if(!existingItem) {
                 state.items.push({id: newItem.id, price: newItem.price, quantity: 1, totalPrice: newItem.price, name: newItem.title})
             } else {
@@ -22,6 +28,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if(existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
             } else {
@@ -31,6 +38,7 @@ const cartSlice = createSlice({
         }
     }
 });
+
 
 export const cartActions = cartSlice.actions;
 
