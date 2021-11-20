5b9e6288a47de31c717b3ac5d3fe508fec0009cb
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-11-20 19:57:22 +0200

    react router basics part 2

--- React-router-basics/src/App.js
+++ React-router-basics/src/App.js
@@ -1,6 +1,7 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
@@ -8,12 +9,20 @@ function App() {
     <div>
       <MainHeader />
       <main>
+      <Switch>
+      <Route path="/" exact>
+        <Redirect to="/welcome" />
+      </Route>
       <Route path="/welcome">
         <Welcome />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Products />
       </Route>
+      <Route path="/products/:productId">
+        <ProductDetail />
+      </Route>
+      </Switch>
       </main>
     </div>
   );
