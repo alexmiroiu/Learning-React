7bab5bbfb2832fe3bfc68b5c945cb6bb7ed64f6d
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-11-21 19:35:37 +0200

    react router project partea a 2a

--- React-router-project/src/components/quotes/QuoteForm.js
+++ React-router-project/src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,8 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+
+  const [isEnteringData, setIsEnteringData] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,9 +22,19 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const formFocusedHandler = () => {
+    setIsEnteringData(true);
+  }
+
+  const finishedEntering = () => {
+    setIsEnteringData(false);
+  }
+
   return (
+    <Fragment>
+    <Prompt when={isEnteringData} message={(location) => 'Are you sure you want to leave? All your entered data will be lost!'}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +50,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishedEntering} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
