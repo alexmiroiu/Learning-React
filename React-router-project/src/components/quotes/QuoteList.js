3460a7e12f629c9ee8c3a758b9b12a8b9ea295ec
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-11-22 09:22:14 +0200

    React router minor fix

--- React-router-project/src/components/quotes/QuoteList.js
+++ React-router-project/src/components/quotes/QuoteList.js
@@ -1,11 +1,22 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=asc');
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
